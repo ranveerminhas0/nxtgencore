a7c0629467bdf9136a408b927016f20dec2511c3
ranveerminhas0 <ranveerminhas@proton.me>
2026-02-11 01:05:17 +0530

    dns config

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,7 @@
 import "dotenv/config";
+import dns from "node:dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
